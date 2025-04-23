7ceda555267310eb71009e34e2d8d3c8e6fbacab
D. R. R creativity <141930563+DRRcoder765@users.noreply.github.com>
2025-04-24 00:11:04 +0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AnimeZone from "./components/AnimeZone";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AnimeZone />
+  </React.StrictMode>
+);
